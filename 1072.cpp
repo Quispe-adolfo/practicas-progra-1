f629ce4fa6f9437e48eb48eab6612d1ee0d3d05e
LorenaTorrez <161449777+LorenaTorrez@users.noreply.github.com>
2024-03-06 17:03:39 -0400

    1072_Beecrowd.cpp

--- 1072.cpp
+++ 1072.cpp
@@ -2,20 +2,21 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int x, a;
-    
-    int in = 0;
-    int out = 0;
+    int Intervalo = 0;
+    int FueraDeIntervalo = 0;
     
     cin >> x;
-    for(int i = 0; i < x; i++){
+    for(int i = 0; i < x; i++)
+    {
             cin >> a;
-            if(a >= 10 && a <= 20) in++;
-            else out++;
+            if(a >= 10 && a <= 20) Intervalo++;
+            else FueraDeIntervalo++;
     }
-    cout << in << " in\n";
-    cout << out << " out\n";
+    cout << Intervalo << " in\n";
+    cout << FueraDeIntervalo << " out\n";
     
     return 0;
 }
