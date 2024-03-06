6773ccf7ee586b405ff7527e846198c7ee8a3f3a
LorenaTorrez <161449777+LorenaTorrez@users.noreply.github.com>
2024-03-06 17:01:15 -0400

    Ejercicio_01_02.cpp

--- Ejercicio_01_02.cpp
+++ Ejercicio_01_02.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
 int main() 
 {
-    // Definir el arreglo constante de 10 elementos
-    const int tamano_arreglo = 10;
-    int arreglo[tamano_arreglo] = {2, -3, 4, -5, 6, -7, 8, -9, 10, -11};
-
+    // Definir el tamaño del arreglo
+    const int TamanioArreglo = 10;
+    int Arreglo[TamanioArreglo];
+    // Solicitar al usuario que ingrese los valores del arreglo
+    cout << "Por favor, ingrese " << TamanioArreglo << " numeros enteros por favor: ";
+    for (int i = 0; i < TamanioArreglo; ++i) 
+    {
+        cin >> Arreglo[i];
+    }
     // Variables para contar los números pares positivos y los impares negativos
-    float pares_positivos = 0; // Cambiado a float
-    float impares_negativos = 0; // Cambiado a float
-
+    float Pares = 0; 
+    float Impares = 0; 
     // Iterar sobre el arreglo para contar los números pares positivos e impares negativos
-    for (int i = 0; i < tamano_arreglo; ++i) {
-        if (arreglo[i] > 0 && arreglo[i] % 2 == 0) {
-            pares_positivos++;
-        } else if (arreglo[i] < 0 && arreglo[i] % 2 != 0) {
-            impares_negativos++;
+    for (int i = 0; i < TamanioArreglo; ++i) 
+    {
+        if (Arreglo[i] > 0 && Arreglo[i] % 2 == 0) 
+        {
+            Pares++;
+        } 
+        else if (Arreglo[i] < 0 && Arreglo[i] % 2 != 0) 
+        {
+            Impares++;
         }
     }
-
     // Calcular el porcentaje
-    float porcentaje_pares_positivos = (pares_positivos / tamano_arreglo) * 100; // No se necesita static_cast
-    float porcentaje_impares_negativos = (impares_negativos / tamano_arreglo) * 100; // No se necesita static_cast
-
+    float PorcentajePares = (Pares / TamanioArreglo) * 100;
+    float PorcentajeNegativo = (Impares / TamanioArreglo) * 100;  
     // Imprimir resultados
-    std::cout << "Porcentaje de numeros pares positivos: " << porcentaje_pares_positivos << "%" << std::endl;
-    std::cout << "Porcentaje de numeros impares negativos: " << porcentaje_impares_negativos << "%" << std::endl;
+    cout << "Porcentaje de numeros pares positivos: " << PorcentajePares << "%" << endl;
+    cout << "Porcentaje de numeros impares negativos: " << PorcentajeNegativo << "%" << endl;
 
     return 0;
 }
